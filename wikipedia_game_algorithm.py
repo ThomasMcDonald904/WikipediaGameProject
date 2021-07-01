37e7cf892bec7a6d0b071f27584759ddaed762d5
ThomasMcDonald904 <naynaytom3000@gmail.com>
2021-07-01 13:22:43 -0400

    Remade the Ratings System to not Offset Ratings and Synsets

--- wikipedia_game_algorithm.py
+++ wikipedia_game_algorithm.py
@@ -9,7 +9,7 @@ class WikipideaGameAlgorithm:
 
     def __init__(self, end_url):
         end_word = re.sub('https://en.wikipedia.org/wiki/', '', end_url)
-        self.end_url = end_url.lower()
+        self.end_url = end_url
         try:
             self.end_word_synset = wordnet.synsets(end_word)[0]
         except:
@@ -30,7 +30,7 @@ class WikipideaGameAlgorithm:
 
         cleaned_links = []
         for link in starting_web_links:
-            if re.search('^/wiki/[^:#%_]*$', link) and not re.search('_\(identifier\)', link):
+            if re.search('^/wiki/[^:#%]*$', link) and not re.search('_\(identifier\)', link):
                 cleaned_links.append(link)
         word_links = []
         for link in cleaned_links:
@@ -39,23 +39,18 @@ class WikipideaGameAlgorithm:
 
         link_synsets = []
         for link in word_links:
-            if not wordnet.synsets(link) == []:
-                link_synsets.append(wordnet.synsets(link)[0])
-            else:
-                #print("Word Not In WordNet Library")
-                word_links.remove(link)
+            result = wordnet.synsets(link)
+            if result != []:
+                link_synsets.append([link, result[0]])
+
 
         ratings = []
-        for link in link_synsets:
-            ratings.append(self.end_word_synset.wup_similarity(link))
 
-        synset_ratings = zip(word_links, ratings)
-        synset_ratings = list(synset_ratings)
+        for item in link_synsets:
+            rating = self.end_word_synset.wup_similarity(item[1])
+            ratings.append([item[0], rating])
 
-        for i in range(len(synset_ratings)):
-            synset_ratings[i] = list(synset_ratings[i])
-
-        synset_ratings.sort(key=lambda l:l[1], reverse=True)
+        ratings.sort(key=lambda l:l[1], reverse=True)
 
         # best_link = str(synset_ratings[0][0].name())
         # link_name = re.sub('\.n\.0\d+', '', best_link)
@@ -63,9 +58,9 @@ class WikipideaGameAlgorithm:
         full_best_url = ""
         index = 0
         best_link_rating = 0
-        while (full_best_url == "" or full_best_url in past_links) and index < len(synset_ratings):
-            best_link = synset_ratings[index][0]
-            best_link_rating = synset_ratings[index][1]
+        while (full_best_url == "" or full_best_url in past_links) and index < len(ratings):
+            best_link = ratings[index][0]
+            best_link_rating = ratings[index][1]
             # link_name = re.sub('\.n\.0\d+', '', best_link)
             full_best_url = f"https://en.wikipedia.org/wiki/{best_link}"
             index += 1
@@ -76,16 +71,18 @@ class WikipideaGameAlgorithm:
         
             
 
-    def get_path_to_end_link(self, url, print_path=True):
+    def get_path_to_end_link(self, url, print_path=True, chart=True):
         """Method To Get Path from URL To Final URL """
         current_url = url
-        past_links = [url]
+        past_links = [current_url]
         while not current_url == self.end_url and not current_url == "":
             current_url, current_url_rating = self.get_most_similar_link(current_url, past_links)
             if current_url != "":
                 past_links.append(current_url)
-            if print_path:
+            if print_path and chart:
                 print(current_url + " - " + str(round(current_url_rating, 4)))
+            
+
         if current_url == "":
             print("End URL could not be reached :-(")
         else:
